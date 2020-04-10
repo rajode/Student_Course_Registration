2dc47e42d7eb8ef77df51780294cfd0f55cf5dd3
Abid-Al-Labib <getabidallabib@gmail.com>
2020-04-09 21:50:37 -0600

    Uploading code

---
+++ Course.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class Course {
+
+    private String courseName;
+    private int courseNum;
+    private ArrayList<Course> preReq;
+    private ArrayList<CourseOffering> offeringList;
+
+    public Course(String courseName, int courseNum) {
+        this.setCourseName(courseName);
+        this.setCourseNum(courseNum);
+        // Both of the following are only association ???
+        preReq = new ArrayList<Course>();
+        offeringList = new ArrayList<CourseOffering>();
+    }
+
+    public void addOffering(CourseOffering offering) {
+        if (offering != null && offering.getTheCourse() == null) {
+            offering.setTheCourse(this);
+            if (!offering.getTheCourse().getCourseName().equals(courseName)
+                    || offering.getTheCourse().getCourseNum() != courseNum) {
+                System.err.println("Error! This section belongs to another course!");
+                return;
+            }
+
+            offeringList.add(offering);
+        }
+    }
+
+    public ArrayList<Course> getPreReq() {
+        return preReq;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public void setCourseNum(int courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    @Override
+    public String toString() {
+        String st = "\n";
+        st += getCourseName() + " " + getCourseNum();
+        if (preReq.size() > 0) {
+            st += "\nPrerequisites: ";
+            for (int i = 0; i < preReq.size(); i++) {
+                st += preReq.get(i).getCourseName()+" "+preReq.get(i).getCourseNum() + ", ";
+            }
+            st += "\n";
+        }
+        st += "\nAll course sections:\n";
+        for (CourseOffering c : offeringList)
+            st += c;
+        st += "\n-------\n";
+        return st;
+    }
+
+    public CourseOffering getCourseOfferingAt(int i) {
+        // TODO Auto-generated method stub
+        if (i < 0 || i >= offeringList.size())
+            return null;
+        else
+            return offeringList.get(i);
+    }
+
+    public void addPre(Course a) {
+        preReq.add(a);
+    }
+}
